8bbb409a9ee8cb0213842ad53b541f7a2570b960
D3C0D1NG <joseph.s.foster@icloud.com>
2023-11-14 18:57:52 -0600

    adding content to files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 // Bringing in the required import from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,20 +1,65 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import { validateEmail } from '../utils/helpers';
 export default function Contact() {
-  return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
-    </div>
-  );
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'userName') {
+      setName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleValidation = (e) => {
+    if (e.target.name === 'email') {
+      if (!validateEmail(e.target.value))
+        setErrorMessage('Invalid Email')
+      else { setErrorMessage('')}
+    } else {
+      if (!e.target.value.length)
+        setErrorMessage('Required Field')
+      else { setErrorMessage('')}
+    }
+  }
+
+const handleFormSubmit = (e) => {
+  e.preventDefault()
+  setEmail('');
+  setName('');
+  setMessage('');
+  setErrorMessage('Email Sent');
 }
+
+return (
+  <Form>
+    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+      <Form.Label>Name</Form.Label>
+      <Form.Control name='name' value={name} onChange={handleInputChange} onBlur={handleValidation} type="text" placeholder="First and Last Name" />
+    </Form.Group>
+    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+      <Form.Label>Email</Form.Label>
+      <Form.Control name='email' value={email} onChange={handleInputChange} onBlur={handleValidation} type="email" placeholder="name@example.com" />
+    </Form.Group>
+    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+      <Form.Label>Your message here...</Form.Label>
+      <Form.Control as="textarea" rows={3} />
+    </Form.Group>
+    <Button onClick={handleFormSubmit} variant="primary" type="submit">
+      Submit
+    </Button>
+  </Form>
+);
