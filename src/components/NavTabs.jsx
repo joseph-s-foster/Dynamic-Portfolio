8d629e978c421681259472a1ff9764fe7a034bd3
D3C0D1NG <joseph.s.foster@icloud.com>
2023-11-18 17:08:36 -0600

    styling pages

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,50 +1,47 @@
 import { Link, useLocation } from 'react-router-dom';
 
-// Here we are using object destructuring assignment to pluck off our variables from the props object
-// We assign them to their own variable names
 function NavTabs() {
   const currentPage = useLocation().pathname;
 
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <Link
-          to="/"
-          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
-        >
-          Home
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/Portfolio"
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
-        >
-          Portfolio
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/Resume"
-          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
-        >
-          Resume
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/Contact"
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </Link>
-      </li>
-    </ul>
+    <header className="header d-flex align-items-center justify-content-center flex-column">
+      <h1 className="mb-0">Joe Foster</h1>
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <Link
+            to="/"
+            className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+          >
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/Portfolio"
+            className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
+          >
+            Portfolio
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/Resume"
+            className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
+          >
+            Resume
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/Contact"
+            className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
+          >
+            Contact
+          </Link>
+        </li>
+      </ul>
+      <div className="white-line"></div>
+    </header>
   );
 }
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
 import { validateEmail } from '../utils/helpers';
+
 export default function Contact() {
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
@@ -9,12 +10,10 @@ export default function Contact() {
   const [errorMessage, setErrorMessage] = useState('');
 
   const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
     const { target } = e;
     const inputType = target.name;
     const inputValue = target.value;
 
-    // Based on the input type, we set the state of either email, username, and password
     if (inputType === 'email') {
       setEmail(inputValue);
     } else if (inputType === 'name') {
@@ -26,46 +25,77 @@ export default function Contact() {
 
   const handleValidation = (e) => {
     if (e.target.name === 'email') {
-      if (!validateEmail(e.target.value))
-        setErrorMessage('Invalid Email')
-      else { setErrorMessage('') }
+      if (!validateEmail(e.target.value)) setErrorMessage('Invalid Email');
+      else setErrorMessage('');
     } else {
-      if (!e.target.value.length)
-        setErrorMessage('Required Field')
-      else { setErrorMessage('') }
+      if (!e.target.value.length) setErrorMessage('Required Field');
+      else setErrorMessage('');
     }
-  }
+  };
 
   const handleFormSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     setEmail('');
     setName('');
     setMessage('');
     setErrorMessage('Email Sent');
-  }
+  };
+
+  const containerStyles = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh', // Adjust the height as needed
+  };
+
+  const formContainerStyles = {
+    width: '50%',
+    maxWidth: '400px', // Adjust the max-width as needed
+  };
 
   return (
-    <>
-      <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Name</Form.Label>
-          <Form.Control name='name' value={name} onChange={handleInputChange} onBlur={handleValidation} type="text" placeholder="First and Last Name" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Email</Form.Label>
-          <Form.Control name='email' value={email} onChange={handleInputChange} onBlur={handleValidation} type="email" placeholder="name@example.com" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Your message here...</Form.Label>
-          <Form.Control name='message' value={message} onChange={handleInputChange} onBlur={handleValidation} as="textarea" rows={3} />
-        </Form.Group>
-        <Button onClick={handleFormSubmit} variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-      {errorMessage && <div>
-        {errorMessage}
-      </div>}
-    </>
+    <div style={containerStyles}>
+      <div style={formContainerStyles}>
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              name="name"
+              value={name}
+              onChange={handleInputChange}
+              onBlur={handleValidation}
+              type="text"
+              placeholder="First and Last Name"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              name="email"
+              value={email}
+              onChange={handleInputChange}
+              onBlur={handleValidation}
+              type="email"
+              placeholder="name@example.com"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Your message here...</Form.Label>
+            <Form.Control
+              name="message"
+              value={message}
+              onChange={handleInputChange}
+              onBlur={handleValidation}
+              as="textarea"
+              rows={3}
+            />
+          </Form.Group>
+          <Button onClick={handleFormSubmit} variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        {errorMessage && <div>{errorMessage}</div>}
+      </div>
+    </div>
   );
 }
