94ee015957c4e1338846aa4cb0eaeeb02bb9f35f
D3C0D1NG <joseph.s.foster@icloud.com>
2023-11-19 20:44:55 -0600

    styled content

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,23 +4,25 @@ function Project({ project }) {
   const { name, repo, link, description, image } = project;
 
   return (
-    <div className="p-3 col-md-6 col-lg-4">
-      <img
-        src={projects[image]}
-        alt={name}
-        className="p-img img-fluid"
-        style={{ maxWidth: '80%', height: 'auto' }}
-      />
-      <div className="mt-3">
-        <h1>
-          <a href={link} style={{ fontSize: '1.5rem' }}>
-            {name}
-          </a>{' '}
-          <a href={repo}>
-            <i className="fab fa-github"></i>
-          </a>
-        </h1>
-        <p style={{ width: '100%' }}>{description}</p>
+    <div className="container mx-auto p-3 col-md-6 col-lg-4 d-flex justify-content-center">
+      <div className="mx-auto">
+        <img
+          src={projects[image]}
+          alt={name}
+          className="p-img img-fluid"
+          style={{ maxWidth: '300px', height: 'auto' }}
+        />
+        <div className="mt-3">
+          <h1>
+            <a href={link} style={{ fontSize: '1.5rem', color: 'white' }}>
+              {name}
+            </a>{' '}
+            <a href={repo}>
+              <i className="fab fa-github"></i>
+            </a>
+          </h1>
+          <p>{description}</p>
+        </div>
       </div>
     </div>
   );
