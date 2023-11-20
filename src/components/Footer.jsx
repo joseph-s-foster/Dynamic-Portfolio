dfca2452c28531cfa2f9a4d17a9a44afc8d2131d
D3C0D1NG <joseph.s.foster@icloud.com>
2023-11-20 08:30:14 -0600

    added github link in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
   return (
-    <footer className='footer'>
-      &copy; Joe Foster 2023
+    <footer className="footer text-center">
+      &copy; Joe Foster 2023{" "}
+      <a
+        href="https://github.com/joseph-s-foster"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="/src/assets/project/github.png"
+          alt="GitHub Logo"
+          className="footer-icon"
+        />
+      </a>
     </footer>
   );
 }
 
 export default Footer;
-
