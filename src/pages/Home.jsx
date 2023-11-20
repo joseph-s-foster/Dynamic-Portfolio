96ffb61ec79f9b90d7d5e477ce20b631afd56a96
D3C0D1NG <joseph.s.foster@icloud.com>
2023-11-20 07:41:48 -0600

    added home content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,26 @@
-export default function Home() {
+import Project from "../components/Project";
+
+function Portfolio() {
+  // fill in the required data, image file should be in the assets/projects folder and name should match the "default as " statement in index.js
+  const projects = [
+    {
+      description: "Full-Stack Developer",
+      image: "avatar",
+    },
+    // Add more projects as needed
+  ];
+
+  // for each project, use the Project component to build a project
   return (
-    <div>
-      <h1>Home</h1>
-      <p>
-        Photo and desc required.
-      </p>
+    <div className="container d-flex justify-content-center align-items-center flex-column vh-50">
+        {projects.map((project, index) => (
+          <div key={"project-" + index} className="col-md-6 col-lg-4 mb-4">
+            <Project project={project} className="mx-auto" />
+          </div>
+        ))}
     </div>
   );
 }
+
+export default Portfolio;
+
