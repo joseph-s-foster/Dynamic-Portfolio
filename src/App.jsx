a16e14cbbb14250509ac5c7e5e6bb5975fe76beb
D3C0D1NG <joseph.s.foster@icloud.com>
2023-11-19 18:43:01 -0600

    added footer.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-// Bringing in the required import from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
+import Footer from './components/Footer'; // Import your Footer component
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-import './App.css'; // Import your custom CSS file
+import './App.css';
 
 function App() {
-  // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
       <Nav />
       <main className="main-container">
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      &copy; Joe Foster 2023
+    </footer>
+  );
+}
+
+export default Footer;
+
