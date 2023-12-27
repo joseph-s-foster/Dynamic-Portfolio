7059439dad4588fab19f738f3634fe670200febf
D3C0D1NG <joseph.s.foster@icloud.com>
2023-12-27 10:22:34 -0600

    generated hook for home page h2

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,28 +1,23 @@
-import * as projects from '../assets';
-import '../Portfolio.css'
+import "../Portfolio.css";
 
 function Project({ project }) {
-  const { name, repo, link, description, image } = project;
+  const { name, link, description} = project;
 
   return (
     <a href={link} className="project-link">
-      <div className="">
-        <div className="project-content">
-          <div className="project-info">
-            <h1>
-            {name}
-            </h1>
-            <div className='project-description'>{description}</div>
-          </div>
-          {/* <div className="github-link">
+      <div className="project-content">
+        <div className="project-info">
+          <h1>{name}</h1>
+          <div className="project-description">{description}</div>
+        </div>
+        {/* <div className="github-link">
             <a href={repo}>
               <i className="fab fa-github"></i>
             </a>
           </div> */}
-        </div>
       </div>
     </a>
-  );  
+  );
 }
 
 export default Project;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Home() {
+  const [typedText, setTypedText] = useState("");
+  const textToType = "Creating mobile-first experiences";
+
+  useEffect(() => {
+    const typeText = async () => {
+      for (let i = 0; i < textToType.length; i++) {
+        setTypedText((prevText) => prevText + textToType[i]);
+        await new Promise((resolve) => setTimeout(resolve, 100));
+      }
+    };
+
+    typeText();
+  }, [textToType]);
+
   return (
     <div
       style={{
@@ -11,8 +25,8 @@ function Home() {
         height: "80vh",
       }}
     >
-      <h1 style={{ fontSize: "3rem" }}>Joe Foster</h1>
-      <h2 style={{ fontSize: "1.33rem" }}>Full-Stack Developer</h2>
+      <h1 style={{ fontSize: "4rem" }}>Joe Foster</h1>
+      <h2 style={{ fontSize: "1.1rem" }}>{typedText}</h2>
     </div>
   );
 }
