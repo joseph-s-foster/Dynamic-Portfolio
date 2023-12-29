697db69d89e47ed2bdeaa8bcf7bd509f9bd9f580
D3C0D1NG <joseph.s.foster@icloud.com>
2023-12-28 21:10:41 -0600

    added cursor logic to the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,42 @@ import React, { useState, useEffect } from "react";
 
 function Home() {
   const [typedText, setTypedText] = useState("");
-  const textToType = "ull-Stack Web Developer";
+  const [showCursor, setShowCursor] = useState(true);
+  const textToType = "edX Full-Stack";
 
   useEffect(() => {
-    const typeText = async () => {
+    const typeAndBackspace = async () => {
+      const cursorFlashInterval = setInterval(() => {
+        setShowCursor((prev) => !prev);
+      }, 500); // Flash interval in milliseconds
+
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      
+      // Type out
       for (let i = 0; i < textToType.length; i++) {
         setTypedText((prevText) => prevText + textToType[i]);
         await new Promise((resolve) => setTimeout(resolve, 100));
       }
+
+      // Wait for a moment before backspacing
+      await new Promise((resolve) => setTimeout(resolve, 2600));
+
+      // Backspace
+      for (let i = textToType.length; i >= 0; i--) {
+        setTypedText((prevText) => prevText.slice(0, -1));
+        await new Promise((resolve) => setTimeout(resolve, 100));
+      }
+
+      await new Promise((resolve) => setTimeout(resolve, 400));
+
+      // Clear the interval to stop flashing when not needed
+      clearInterval(cursorFlashInterval);
+
+      // Repeat the process
+      typeAndBackspace();
     };
 
-    typeText();
+    typeAndBackspace();
   }, [textToType]);
 
   return (
@@ -26,7 +51,11 @@ function Home() {
       }}
     >
       <h1 style={{ fontSize: "2.5rem" }}>Joe Foster</h1>
-      <h2 style={{ fontSize: "16px" }}>F{typedText}</h2>
+      <h2 style={{ fontSize: "16px", position: "relative" }}>
+        &nbsp;{typedText}
+        <span style={{ opacity: showCursor ? 1 : 0 }}>|</span>
+        &nbsp;
+      </h2>
     </div>
   );
 }
