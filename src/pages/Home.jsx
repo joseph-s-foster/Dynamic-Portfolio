9bcb87197a87e10a8d3aca5ba8fa1b68ed7b7f74
D3C0D1NG <joseph.s.foster@icloud.com>
2024-01-02 19:30:57 -0600

    created tag loop on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ function Home() {
   const [typedText, setTypedText] = useState("");
   const [showCursor, setShowCursor] = useState(true);
   const textToType = "edX Full-Stack";
+  const texttoType2 = "UX-driven";
+  // const texttoType3 = "AWS";
 
   useEffect(() => {
     const typeAndBackspace = async () => {
@@ -11,8 +13,8 @@ function Home() {
         setShowCursor((prev) => !prev);
       }, 500); // Flash interval in milliseconds
 
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-      
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
       // Type out
       for (let i = 0; i < textToType.length; i++) {
         setTypedText((prevText) => prevText + textToType[i]);
@@ -20,7 +22,7 @@ function Home() {
       }
 
       // Wait for a moment before backspacing
-      await new Promise((resolve) => setTimeout(resolve, 2600));
+      await new Promise((resolve) => setTimeout(resolve, 1000));
 
       // Backspace
       for (let i = textToType.length; i >= 0; i--) {
@@ -28,7 +30,39 @@ function Home() {
         await new Promise((resolve) => setTimeout(resolve, 100));
       }
 
-      await new Promise((resolve) => setTimeout(resolve, 600));
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      // Type out
+      for (let i = 0; i < texttoType2.length; i++) {
+        setTypedText((prevText) => prevText + texttoType2[i]);
+        await new Promise((resolve) => setTimeout(resolve, 100));
+      }
+
+      // Wait for a moment before backspacing
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      // Backspace
+      for (let i = texttoType2.length; i >= 0; i--) {
+        setTypedText((prevText) => prevText.slice(0, -1));
+        await new Promise((resolve) => setTimeout(resolve, 100));
+      }
+
+      // await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      // // Type out
+      // for (let i = 0; i < texttoType3.length; i++) {
+      //   setTypedText((prevText) => prevText + texttoType3[i]);
+      //   await new Promise((resolve) => setTimeout(resolve, 100));
+      // }
+
+      // // Wait for a moment before backspacing
+      // await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      // // Backspace
+      // for (let i = texttoType3.length; i >= 0; i--) {
+      //   setTypedText((prevText) => prevText.slice(0, -1));
+      //   await new Promise((resolve) => setTimeout(resolve, 100));
+      // }
 
       // Clear the interval to stop flashing when not needed
       clearInterval(cursorFlashInterval);
