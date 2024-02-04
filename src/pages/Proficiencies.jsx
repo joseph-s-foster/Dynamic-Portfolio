9bdcf5fcd716a381458abe733ae7b7d0acc71f6a
D3C0D1NG <joseph.s.foster@icloud.com>
2024-02-04 16:24:34 -0600

    styling proficiencies content

--- src/pages/Proficiencies.jsx
+++ src/pages/Proficiencies.jsx
@@ -1,63 +1,94 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import '../Proficiencies.css'
 import background from "../assets/project/background.png";
 import caret from "../assets/project/caret.svg";
+import html from "../assets/project/html.svg";
+import css from "../assets/project/css.svg";
+import javascript from "../assets/project/javascript.svg";
+import jquery from "../assets/project/jquery.svg";
+import git from "../assets/project/git.svg";
+import node from "../assets/project/node.svg";
+import express from "../assets/project/express.svg";
+import mysql from "../assets/project/mysql.svg";
+import handlebars from "../assets/project/handlebars.svg";
+import mongodb from "../assets/project/mongodb.svg";
+import react from "../assets/project/react.svg";
+import graphql from "../assets/project/graphql.svg";
+import python from "../assets/project/python.svg";
 
-function Proficiences() {
+function Proficiencies() {
   const handleScroll = (event) => {
     event.preventDefault();
 
-    const projectsContainer = document.getElementById("projects");
-    if (projectsContainer) {
-      projectsContainer.scrollIntoView({ behavior: "smooth" });
+    const iconsContainer = document.getElementById("icons");
+    if (iconsContainer) {
+      iconsContainer.scrollIntoView({ behavior: "smooth" });
     }
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-        backgroundImage: `url(${background})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
+    <>
       <div
         style={{
-          position: "absolute",
-          bottom: "35%",
-          left: "12%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+          backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          position: "relative", // Make sure it's positioned relative
         }}
       >
-        <h1
+        <div
           style={{
-            fontSize: "2.5rem",
+            position: "absolute",
+            bottom: "35%",
+            left: "12%",
           }}
         >
-          Explore client <br />
-          and server-side <br />
-        </h1>
-        <h2 style={{ fontSize: "2.5rem" }} className="cta">
-          capabilities
-          <a href="#projects" onClick={handleScroll}>
-            <img
-              src={caret}
-              alt="downward caret"
-              style={{
-                transform: "rotate(90deg)",
-                margin: "6px",
-                height: "42px",
-                width: "42px",
-              }}
-            />
-          </a>
-        </h2>
+          <h1
+            style={{
+              fontSize: "2.5rem",
+            }}
+          >
+            Explore client <br />
+            and server-side <br />
+          </h1>
+          <h2 style={{ fontSize: "2.5rem" }} className="cta">
+            capabilities
+            <a href="#icons" onClick={handleScroll}>
+              <img
+                src={caret}
+                alt="downward caret"
+                style={{
+                  transform: "rotate(90deg)",
+                  margin: "6px",
+                  height: "42px",
+                  width: "42px",
+                }}
+              />
+            </a>
+          </h2>
+        </div>
       </div>
-    </div>
+      <div className="icons" id="icons">
+        <img src={html} alt="HTML" />
+        <img src={css} alt="CSS" />
+        <img src={javascript} alt="JavaScript" />
+        <img src={jquery} alt="jQuery" />
+        <img src={node} alt="Node.js" />
+        <img src={express} alt="Express.js" />
+        <img src={mysql} alt="MySQL" />
+        <img src={handlebars} alt="Handlebars" />
+        <img src={mongodb} alt="MongoDB" />
+        <img src={react} alt="React" />
+        <img src={graphql} alt="GraphQL" />
+        <img src={python} alt="Python" />
+      </div>
+    </>
   );
 }
 
-export default Proficiences;
+export default Proficiencies;
