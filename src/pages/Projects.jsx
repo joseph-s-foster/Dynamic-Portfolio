56b046ffd135d1d932781b4bbbb4a9dfb768d0fa
joseph-s-foster <135551185+joseph-s-foster@users.noreply.github.com>
2024-06-07 06:28:24 -0500

    Update proficiencies element border radius

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -73,7 +73,7 @@ const Component = ({ projectsGroup1 }) => {
                 padding: "12px",
                 fontSize: "1rem",
                 border: "solid #dddddd 1px",
-                borderRadius: "6px",
+                borderRadius: "4px",
                 cursor: "pointer",
               }}
               className="explore"
