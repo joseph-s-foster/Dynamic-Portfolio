eb3b7ef3e39deaba633cda5daad1a2445897ede0
D3C0D1NG <joseph.s.foster@icloud.com>
2024-06-08 16:17:46 -0500

    loading hook testing complete, performs as expected

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Footer from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "./App.css";
