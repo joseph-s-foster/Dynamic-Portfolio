868eddbf48cc19b27d8e981c80fcbff36f2cbe66
D3C0D1NG <joseph.s.foster@icloud.com>
2025-04-10 17:57:28 -0500

    styling bg for mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -113,7 +113,18 @@ function Home() {
       </div>
       {isLoading && <LoadingSpinner />}
       <div>
-        <div className="container">
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100svh",
+            backgroundImage: `url(${background})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
           <div className="interact1">
             <h1 style={{ fontSize: "3rem", paddingBottom: "4px" }}>
               Joseph Foster
